3f63ed53e7b7b19afbb27b8978dac5d6a485b2db
Saran Kumar <nvskumar2003@gmail.com>
2024-11-13 21:35:08 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+//index.js
+// mongodb+srv://sathwiknarkedimilli29:root@cluster0.gyekfu2.mongodb.net/
+const connectToMongo = require("./db");
+const express = require("express");
+const cors = require("cors");
+
+connectToMongo();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = 5000;
+
+// available routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/courses", require("./routes/course"));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
